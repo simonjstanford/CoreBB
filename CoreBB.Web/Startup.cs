03a85144de154384ab76b3d8cd293629ff3a07b2
Simon Stanford <simonjstanford@gmail.com>
2018-05-27 08:46:28 +0100

    Configured web app for MVC

--- CoreBB.Web/Startup.cs
+++ CoreBB.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,20 +16,25 @@ namespace CoreBB
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(option => {
+                    option.LoginPath = "/User/LogIn";
+                    option.AccessDeniedPath = "/Error/AccessDenied";
+                    option.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseExceptionHandler("/Error/Index");
+            app.UseStaticFiles();   
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();  //The default URL routing template is {controller=Home}/{action=Index}/{id?}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
         }
     }
 }
