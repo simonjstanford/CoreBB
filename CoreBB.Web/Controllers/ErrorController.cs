618b8da3b42fa92f30afcabfbc49a3671176b9b7
Simon Stanford <simonjstanford@gmail.com>
2018-06-01 20:07:12 +0100

    Added home and error views.

---
+++ CoreBB.Web/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBB.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            ViewData["StatusCode"] = HttpContext.Response.StatusCode;
+            ViewData["Message"] = exception.Error.Message;
+            ViewData["StackTrace"] = exception.Error.StackTrace;
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
