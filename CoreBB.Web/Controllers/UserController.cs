d78fa614c83d2a491cbdf75754fd16feed5605a5
Simon Stanford <simonjstanford@gmail.com>
2018-06-03 10:24:39 +0100

    Added user controller and register view model

---
+++ CoreBB.Web/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreBB.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBB.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private CoreBBContext coreBBContext;
+
+        public UserController(CoreBBContext coreBBContext)
+        {
+            this.coreBBContext = coreBBContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous, HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return View();
+        }
+    }
+}
---
+++ CoreBB.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreBB.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required, DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required, DisplayName("Password")]
+        public string Password { get; set; }
+
+        [Required, DisplayName("Repeat Password")]
+        public string RepeatPassword { get; set; }
+
+        [Required, DisplayName("Self-Introduction")]
+        public string Description { get; set; }
+    }
+}
